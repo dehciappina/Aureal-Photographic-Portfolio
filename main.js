77d6ea7e7ecb7d877aad41f806a8a3a0990e0812
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2018-05-20 12:36:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,16 @@
 
 
-$('.section').on('click', function(rotateImages) {
-    $(document).find('.fibona1').removeClass('img_01').addClass('img_02')
-    $(document).find('.fibona2').removeClass('img_02').addClass('img_03')
-    $(document).find('.fibona3').removeClass('img_03').addClass('img_06')
-    $(document).find('.fibona4').removeClass('img_06').addClass('img_05')
-    $(document).find('.fibona5').removeClass('img_05').addClass('img_04')
-    $(document).find('.fibona6').removeClass('img_04').addClass('img_02')
-    $(document).find('.fibona1').removeClass('img_01').addClass('img_01')
+$(document).on('click', '.section', function(rotateImages) {
+
+if(!$(this).hasClass('MAIN_IMG')) {
+
+    $(document).find('.fibona1').toggleClass('img_01').toggleClass('img_02')
+    $(document).find('.fibona2').toggleClass('img_02').toggleClass('img_03')
+    $(document).find('.fibona3').toggleClass('img_03').toggleClass('img_06')
+    $(document).find('.fibona4').toggleClass('img_06').toggleClass('img_05')
+    $(document).find('.fibona5').toggleClass('img_05').toggleClass('img_04')
+    $(document).find('.fibona6').toggleClass('img_04').toggleClass('img_01')
+    $(document).find('.fibona1').toggleClass('img_01').toggleClass('img_01')
+}
 
 })
-
-
-
-/*
-$(document).on('click', '.layer1 article', function() {
-    $(this).toggleClass('full_mode')
-    $('.top_img img, .side_img img').toggleClass('scrollable')
-    $('.layer1').toggleClass('layer1_z_index')
-})
-
-$(document).on('click', '.layer2', function()  {
-    $('.layer1').toggleClass('layer1').toggleClass('layer6')
-    $('.layer2').toggleClass('layer2').toggleClass('layer1')
-    $('.layer3').toggleClass('layer3').toggleClass('layer2')
-    $('.layer4').toggleClass('layer4').toggleClass('layer3')
-    $('.layer5').toggleClass('layer5').toggleClass('layer4')
-    $('.layer6').toggleClass('layer6').toggleClass('layer5')
-})
-
-*/
